39a68aa60842c284c8c19a6e20db012cfcadd3bd
cesarvfonseca <www.cavf.20@gmail.com>
2017-01-26 10:44:33 -0600

    Leer XML y salvar informacion en BD
    XML con estructura erronea aun marca error

---
+++ leerXML/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace leerXML
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLeer = new System.Windows.Forms.Button();
+            this.txtRuta = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnLeer
+            // 
+            this.btnLeer.Location = new System.Drawing.Point(213, 18);
+            this.btnLeer.Name = "btnLeer";
+            this.btnLeer.Size = new System.Drawing.Size(75, 28);
+            this.btnLeer.TabIndex = 0;
+            this.btnLeer.Text = "Leer";
+            this.btnLeer.UseVisualStyleBackColor = true;
+            this.btnLeer.Click += new System.EventHandler(this.btnLeer_Click);
+            // 
+            // txtRuta
+            // 
+            this.txtRuta.Location = new System.Drawing.Point(12, 21);
+            this.txtRuta.Name = "txtRuta";
+            this.txtRuta.Size = new System.Drawing.Size(194, 20);
+            this.txtRuta.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(300, 58);
+            this.Controls.Add(this.txtRuta);
+            this.Controls.Add(this.btnLeer);
+            this.Name = "Form1";
+            this.Text = "Leer XML";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLeer;
+        private System.Windows.Forms.TextBox txtRuta;
+    }
+}
+
---
+++ leerXML/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Serialization;
+using System.Xml.Linq;
+using System.Data.SqlClient;
+
+namespace leerXML
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con = new SqlConnection("Data Source = ATALAYA-STD;" + "Initial Catalog = CSRAPP ; Integrated Security = true; MultipleActiveResultSets=true;");
+        //SqlConnection con = new SqlConnection("Data Source=MEXQ-SERVER4;Initial Catalog=MEXQAppJulio;Persist Security Info=False;User ID=sa;Password=P@ssw0rd; MultipleActiveResultSets=true;");
+        string periodo,ruta,insertaD;
+        string UUID,idNota,folio,serie,fecha,cliente,rfc;
+        SqlCommand comando,cmd;
+        XmlReader reader;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLeer_Click(object sender, EventArgs e)
+        {
+            //try
+            //{
+                UUID = string.Empty;
+                periodo = txtRuta.Text;
+                string query = "EXEC vData '" + periodo + "'";
+                comando = new SqlCommand(query, con);
+                con.Open();
+                SqlDataReader leer = comando.ExecuteReader();
+                if (leer.HasRows)
+                {
+                    while (leer.Read())
+                    {
+                        ruta = leer["Location"].ToString();
+                        idNota = leer["NoteID"].ToString();
+                        reader = XmlReader.Create(ruta);
+                            while (reader.Read())
+                            {
+                                if ((reader.NodeType == XmlNodeType.Element) && (reader.Name == "tfd:TimbreFiscalDigital"))
+                                    {
+                                        if (reader.HasAttributes)
+                                        {
+                                            UUID = reader.GetAttribute("UUID");
+                                            fecha = reader.GetAttribute("FechaTimbrado");
+                                            insertaD = "INSERT into xmldata (UUID,FECHA_TIMBRADO) " +
+                                                " VALUES ('" + UUID + "','" + fecha + "');";
+                                            //insertaD = "insertRecords'" + UUID + "','" + folio + "','" + serie + "','" + fecha + "','" + cliente + "','" + rfc + "','" + idNota + "','" + ruta + "'";
+                                            cmd = new SqlCommand(insertaD, con);
+                                            cmd.ExecuteNonQuery();
+                                        }
+                                    }
+                                }
+                                          
+                            
+                                
+                                //if ((reader.NodeType == XmlNodeType.Element) && (reader.Name == "cfdi:Comprobante"))
+                                //{
+                                //    if (reader.HasAttributes)
+                                //    {
+                                //        folio = reader.GetAttribute("folio");
+                                //        serie = reader.GetAttribute("serie");
+                                //    }
+
+                                //}
+                                //else
+                                //{
+                                //    break;
+                                //}
+                                //if ((reader.NodeType == XmlNodeType.Element) && (reader.Name == "cfdi:Emisor"))
+                                //{
+                                //    if (reader.HasAttributes)
+                                //    {
+                                //        rfc = reader.GetAttribute("rfc");
+                                //        cliente = reader.GetAttribute("nombre");
+                                //    }
+                                //}
+                                //else
+                                //{
+                                //    break;
+                                //}
+                            //}
+                        ruta = string.Empty;
+                    }
+                    MessageBox.Show("Datos guardados exitosamente!!!");
+                }
+                else
+                {
+                    MessageBox.Show("No se encontro información =(");
+                }
+                con.Close();
+                txtRuta.Text = string.Empty;
+            //}
+            //catch (Exception err)
+            //{
+            //    //Console.WriteLine("Error reading from {0}. Message = {1}", ruta);
+            //    Console.WriteLine("error: ", err.Message);
+            //    throw;
+            //}
+            //finally {
+            //    Console.ReadLine();
+            //}
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /*string rutaArchivo = string.Empty;
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                rutaArchivo = ofd.FileName;
+            }
+            txtRuta.Text = rutaArchivo;
+            string query = "SELECT ad.BatNbr,ad.PerPost,"+
+                                    "att.NoteID,att.NameOfFile,att.Location"+
+                                    "FROM CSRAPP.dbo.apdoc ad"+
+                                    "INNER JOIN CSRDEV.dbo.attachment att"+
+                                    "ON ad.NoteID=att.NoteID"+
+                                    "WHERE att.NoteID='3593' AND att.Location LIKE '%.xml'";
+            periodo = txtRuta.Text;
+            string query = "EXEC vData '"+periodo+"'";
+            SqlCommand comando = new SqlCommand(query, con);
+            con.Open();
+            SqlDataReader leer = comando.ExecuteReader();
+            if (leer.Read() == true)
+            {
+                //MessageBox.Show("Registro encontrado");
+                txtRuta.Text = leer["Location"].ToString();
+            }
+            else {
+                //MessageBox.Show("Registro NO encontrado");
+                txtRuta.Text = "";
+            }
+            con.Close();*/
+        }
+    }
+}
